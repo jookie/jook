ce1899dc0e4ded1c1b8e93e3e7f00d44b3fdaffc
jookie <dovpeles@gmail.com>
2024-07-26 14:09:46 -0400

    first commit

---
+++ pub.sh
@@ -0,0 +1,38 @@
+
+# export PATH=/usr/local/share/npm/bin:$PATH
+export REPLICATE_API_TOKEN=r8_YIHBcqbcUo23Sx7ac7274Ib9FdPMcxe0arycC
+# npx convex dev --configure=existing --team jookie --project convex-chat-gpt-0c3a2
+git add .
+git commit -m "Initial commit"
+git push 
+# Go to GitHub and log in.
+# Click on the "New" button to create a new repository.
+# add a description.
+# Do not initialize the repository with a README, .gitignore, or license if you've already initialized your project locally.
+#    ```bash
+#    git remote -v
+#    git remote set-url https://github.com/jookie/PhotoTrack.git
+#    ```
+
+#    ```bash
+#   git init
+# git .add
+#   git commit -m "first commit"
+#   git branch -M main
+#   git remote add origin https://github.com/jookie/PhotoTrack.git
+#   git fast-forwards' in 'git push --help' for details.
+
+#   git push -u origin main
+
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/jookie/captionai.git
+#   git branch -M main
+#   git push -u origin main
+#mm initialize this repository with code from a Subversion, Mercurial, or TFS projec
+
+# processes using "kill PID". 
+# ------------------------------
+# COMMAND  PID     USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+# node    5926 MacAdmin   23u  IPv4 0x9c6759e5df48fca9      0t0  TCP *:e
+
+
